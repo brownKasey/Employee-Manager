50b8f040307f71380ea29a0e630c693dd88aaa32
unknown <kaseybrown28@icloud.com>
2023-07-28 17:28:32 -0400

    added data to seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -3,13 +3,13 @@ CREATE DATABASE company_db;
 
 Use company_db;
 
-CREATE TABLE department(
+CREATE TABLE department (
  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(30) NOT NULL,
+ name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role(
-id INT NOT NULL PRIMARY KEY,
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(9,2) NOT NULL,
 department_id INT NOT NULL,
@@ -17,10 +17,14 @@ FOREIGN KEY (department_id)
 REFERENCES department(id)
 );
 
-CREATE TABLE employee(
+CREATE TABLE employee (
 id INT NOT NULL PRIMARY KEY,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30),
-role_id INT FOREIGN KEY REFERENCES role(id),
-manager_id INT NULL FOREIGN KEY REFERENCES employee(id),
+role_id INT ,
+FOREIGN KEY (role_id)
+REFERENCES role(id),
+manager_id INT NULL,
+FOREIGN KEY (manager_id) 
+REFERENCES employee(id)
 );
