65df29495e7dc963bf8b33ed08486a835d6568ef
unknown <kaseybrown28@icloud.com>
2023-07-31 22:41:41 -0400

    changed server.js to an index.js, added an init function to index.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,33 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
-Use company_db;
+USE company_db;
 
 CREATE TABLE department (
- id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
- name VARCHAR(30) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
 );
 
 CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-title VARCHAR(30) NOT NULL,
-salary DECIMAL(9,2) NOT NULL,
-department_id INT NOT NULL,
-FOREIGN KEY (department_id)
-REFERENCES department(id)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) ,
+    salary DECIMAL(9,2) ,
+    department_id INT,
+     FOREIGN KEY (department_id)
+     REFERENCES department(id)
+     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-id INT NOT NULL PRIMARY KEY,
-first_name VARCHAR(30) NOT NULL,
-last_name VARCHAR(30),
-role_id INT ,
-FOREIGN KEY (role_id)
-REFERENCES role(id),
-manager_id INT NULL,
-FOREIGN KEY (manager_id) 
-REFERENCES employee(id)
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+     FOREIGN KEY(role_id)
+     REFERENCES role(id)
+     ON DELETE SET NULL,
+    manager_id INT,
+     FOREIGN KEY(manager_id)
+     REFERENCES employee(id)
+     ON DELETE SET NULL
 );
